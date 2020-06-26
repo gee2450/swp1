4b5f3f20333633a6212bb217a5a621bc0242ddfd
minji-yun <gee2450@kookmin.ac.kr>
2020-06-26 16:27:49 +0900

    Issue #7 By amending 'a = d.get('a', [''])[0]' to 'a = d.get('a', ['0'])[0]' 'b = d.get('b', [''])[0]' to 'b = d.get('a', ['0'])[0]' solving problem

--- calc/main.py
+++ calc/main.py
@@ -3,9 +3,8 @@ from template import html
 
 def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
-	a = d.get('a', [''])[0]
-  	b = d.get('b', [''])[0]
-	sum, mul = 0, 0
+	a = d.get('a', ['0'])[0]
+  	b = d.get('b', ['0'])[0]
 	try:
 		a, b = int(a), int(b)
 		sum, mul = a+b, a*b
